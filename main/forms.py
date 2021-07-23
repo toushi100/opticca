38994309e1fe2ebbfe086ca8b6abdae29d5fa2f7
Sharif ahmad <sharifahmad2061@users.noreply.github.com>
2021-07-23 16:40:01 +0500

    tried i18n, but haven't tested it (#4)

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 
 from phonenumber_field.formfields import PhoneNumberField
 
@@ -10,7 +11,7 @@ from .models import Person
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    Tel = PhoneNumberField(label='Phone Number')
+    Tel = PhoneNumberField(label=_('Phone Number'))
     class Meta:
         model = Person
         fields = ['first_name', 'last_name', 'username',
--- main/models.py
+++ main/models.py
@@ -6,6 +6,7 @@ from enum import Enum, unique
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.utils.translation import gettext as _
 
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -25,16 +26,16 @@ last_name
 
 class Person(User):
     DoesNotExist = None
-    is_customer = models.BooleanField(default=False)
-    is_supplier = models.BooleanField(default=False)
-    Tel = PhoneNumberField('phone number', null=True, blank=True, unique=True)
+    is_customer = models.BooleanField(_('customer ?'), default=False)
+    is_supplier = models.BooleanField(_('supplier ?'), default=False)
+    Tel = PhoneNumberField(_('phone number'), null=True, blank=True, unique=True)
 
     default_shipping_address = models.ForeignKey("Address", null=True, blank=True, default=None,
-                                                 verbose_name="Default shipping address",
+                                                 verbose_name=_("Default shipping address"),
                                                  related_name="default_shipping_address", on_delete=models.PROTECT)
 
     default_invoicing_address = models.ForeignKey("Address", null=True, blank=True, default=None,
-                                                  verbose_name="Default invoice address",
+                                                  verbose_name=_("Default invoice address"),
                                                   related_name="default_invoicing_address", on_delete=models.PROTECT)
 
     '''
